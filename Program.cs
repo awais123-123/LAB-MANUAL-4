8f28de48908788e0f60eb01ea4c0f6bae5db8a8d
awais123-123 <awer2372@gmail.com>
2025-03-08 01:30:17 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            fraction f1=new fraction (10,100);
+            f1.display();
+        }
+    }
+}
